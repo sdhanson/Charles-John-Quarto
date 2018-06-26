9f3e9244f0992fc3a794f970e9b3c8017582aacf
sdhanson <sdhanson@usc.edu>
2018-06-25 21:46:22 -0500

    Adding jquery for photography page media body -- need to format the fonts and headings and such to be pretty.

--- public/javascripts/carousel.js
+++ public/javascripts/carousel.js
@@ -1,11 +1,10 @@
-// CODE FOR SETTING CAROUSEL IMAGE WHEN IT WAS A CAROUSEL
-// $(".photo-link").click(function() {
-//     $(".carousel-item.active img").attr("src", $(this).find("img").attr("src"));
-// });
-
-
+// links in the grid that is clicked to the modal / carousel media
 $(".photo-link").click(function() {
     $(".modal-container img").attr("src", $(this).find("img").attr("src"));
+    $(".media-body h2.title").text($(this).find(".photo-overlay .photo-overlay-text .title").text());
+    $(".media-body div.location").text($(this).find(".photo-overlay .photo-overlay-text .location").text());
+    $(".media-body div.photographer").text($(this).find(".photo-overlay .photo-overlay-text .photographer").text());
+    $(".media-body div.description").text($(this).find(".photo-overlay .photo-overlay-text .description").text());
+
 });
 
-// USE JQUERY TO SET THE TITLE AND DESCRIPTION IF THERE IS ONE
